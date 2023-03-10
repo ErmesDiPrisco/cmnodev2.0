50f13a64bbc1f83c1eb13f3d61ab38e8c247db57
Dev1 - Matt <m.puleri3@gmail.com>
2023-03-10 11:17:17 +0100

    css aggiornato

--- app.js
+++ app.js
@@ -1,26 +1,14 @@
 const express = require("express")
 const mysql = require("mysql")
-const session = require('express-session');
 const app = express()
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }))
 
+// const errMsg = document.getElementById("err_msg");
+
 const porta = 3000;
 
-app.use(session({
-    secret: 'supersegreto',
-    resave: false,
-    saveUninitialized: true
-  }));
-  
-  const checkAuth = (req, res, next) => {
-    if (!req.session.userId) {
-      res.redirect('/login');
-    } else {
-      next();
-    }
-  };
 const connectionDb = mysql.createConnection({
     user: "root",
     password: "Password01!",
@@ -50,22 +38,22 @@ app.post("/login", (req, res) => {
     var emaill = req.body.email
     var passwordd = req.body.password
     let checks=(args)=>{
+
         for(let i in args){
-            // console.log("Email_DB: " + args[i].email)
-            // console.log("Password_DB: " + args[i].password)
+
             if (args[i].email === emaill && args[i].password === passwordd){
+
                 return true
 
+            }
         }
-    }}
+    }
 
     connectionDb.query(
 
         `select email, password, abilitato from account a \
         inner join account_dipendente ad on ad.id_account = a.id_account \
         inner join dipendente d on d.id_dipendente=ad.id_dipendente`,
-        // where email = ? and password = ?", 
-        // [email, password],
         (err, result) => {
 
             if (err){
@@ -74,13 +62,21 @@ app.post("/login", (req, res) => {
 
             }else{
                 console.log(checks(result))
+
                 if (checks(result)==true){
-                    req.session.userId = 1;
+
                     res.redirect('/home')
+
                 }else{
+
+                    // format.classList.add('error');
                     console.log('campi errati')
-                    res.send('<h1>Hai sbagliato tutto</h1></br>\
-                                <button ><a href="/login">Login</a></button>')
+                    res.send('\
+                        <div style = "text-align: center">\
+                            <h1 style = "font-weight: 200px"> Credenziali Errate </h1>\
+                            </br>\
+                            <button style = "background-color: black; padding: 10px; font-size: 16px; font-weight: bold; border-radius: 15px; cursor: pointer;"><a style = "text-decoration: none; color: white;" href="/login">Login</a></button>\
+                        </div>')
                 }
 
             }
@@ -104,7 +100,3 @@ app.post("/login", (req, res) => {
     // console.log("Password: " + passwordd)
 
 })
-
-app.get('/home', checkAuth, (req, res) => {
-    res.send('Home');
-  });
