638da8f087d7dbf7c4c508fb661daf0590f71900
Ermes <ermesqwerty@gmail.com>
2023-03-20 15:25:10 +0100

    model aggiunti

--- azienda.js
+++ azienda.js
@@ -1,19 +1,29 @@
 const express = require("express");
 const app = express();
 const connectionDb = require("./db");
+var Azienda = require('./models/aziendaModel')
+var id = 'ciao'
+
 // const getAllCompanies = connectionDb.query('select * from azienda', (err, res) => {
 //     if(err) {
 //         return console.log(err);
 //     } else {
-//         console.log(res);
+//         for(i in res){
+            
+//             Azienda=res[i];
+//             console.log(Azienda.id_azienda)
+//         }
 //     }
 // });
 
 const getCompanyById = app.get("/azienda", (req, res) => {
-    res = connectionDb.query(`SELECT * FROM azienda WHERE id_azienda = ?`, [id], function (err, result) {
-        if (err) throw err;
-        console.log(result);
-    }) 
-
-})
+     connectionDb.query(`SELECT * FROM azienda WHERE id_azienda = '${id}' `,(err, res) => {
+            if(err) {
+               return console.log(err);
+          } else {
+                
+                console.log(res)
+                
+            }
+        })});
 
