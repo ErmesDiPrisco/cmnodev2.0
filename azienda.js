b88469d7759b7ed5b9af15f0b957a95ecbbe6337
Dev - Simone <simonebest56@gmail.com>
2023-03-21 15:08:36 +0100

    add get all azienda

--- azienda.js
+++ azienda.js
@@ -3,24 +3,20 @@ const app = express();
 const connectionDb = require("./db");
 const server = require("./server");
 const router = express.Router();
-var Azienda = require("./models/aziendaModel");
+// var Azienda = require("./models/aziendaModel");
 var id = "ciao";
 
 router.get("/", (req, res) => {
-  connectionDb.query(
-    "select * from azienda",
-    (err, res) => {
-      if (err) {
-        return console.log(err);
-      } else {
-        // for (i in res) {
-           Azienda = res;
-        // }
-      }
+  connectionDb.query("SELECT * FROM azienda", (err, rows) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const aziende = rows;
+      res.send(aziende);
     }
-  );
-  res.send(Azienda);
+  });
 });
+
 
 // router.get(`/azienda/:id`, (req, res) => {
 //      connectionDb.query(`SELECT * FROM azienda WHERE id_azienda = '${id}' `,(err, res) => {
