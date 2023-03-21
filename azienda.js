3b6811a670ed7724a780eb02433d27767e709e84
EnricoOlivieri23 <enrico.olivieri2@gmail.com>
2023-03-21 12:43:40 +0100

    add res.send(Azienda)

--- azienda.js
+++ azienda.js
@@ -1,31 +1,36 @@
 const express = require("express");
 const app = express();
 const connectionDb = require("./db");
-const server= require('./server')
+const server = require("./server");
 const router = express.Router();
-var Azienda = require('./models/aziendaModel')
-var id = 'ciao'
+var Azienda = require("./models/aziendaModel");
+var id = "ciao";
 
-// const getAllCompanies = connectionDb.query('select * from azienda', (err, res) => {
-//     if(err) {
-//         return console.log(err);
-//     } else {
-//         for(i in res){
-            
-//             Azienda=res[i];
-//             console.log(Azienda.id_azienda)
-//         }
-//     }
-// });
-router.get(`/azienda/:id`, (req, res) => {
-     connectionDb.query(`SELECT * FROM azienda WHERE id_azienda = '${id}' `,(err, res) => {
-            if(err) {
-               return console.log(err);
-          } else {
-                
-                console.log(res)
-                
-            }
-        })});
+router.get("/", (req, res) => {
+  connectionDb.query(
+    "select * from azienda",
+    (err, res) => {
+      if (err) {
+        return console.log(err);
+      } else {
+        // for (i in res) {
+           Azienda = res;
+        // }
+      }
+    }
+  );
+  res.send(Azienda);
+});
 
-module.exports= router
+// router.get(`/azienda/:id`, (req, res) => {
+//      connectionDb.query(`SELECT * FROM azienda WHERE id_azienda = '${id}' `,(err, res) => {
+//             if(err) {
+//                return console.log(err);
+//           } else {
+
+//                 console.log(res)
+
+//             }
+//         })});
+
+module.exports = router;
