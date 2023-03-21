c3eb92c666d812488c45d931b3724ebd95d3e878
Dev - Simone <simonebest56@gmail.com>
2023-03-21 15:58:11 +0100

    add get id

--- azienda.js
+++ azienda.js
@@ -4,7 +4,7 @@ const connectionDb = require("./db");
 const server = require("./server");
 const router = express.Router();
 // var Azienda = require("./models/aziendaModel");
-var id = "ciao";
+
 
 router.get("/", (req, res) => {
   connectionDb.query("SELECT * FROM azienda", (err, rows) => {
@@ -13,20 +13,39 @@ router.get("/", (req, res) => {
     } else {
       const aziende = rows;
       res.send(aziende);
+      console.log(aziende);
     }
   });
 });
 
 
 // router.get(`/azienda/:id`, (req, res) => {
-//      connectionDb.query(`SELECT * FROM azienda WHERE id_azienda = '${id}' `,(err, res) => {
-//             if(err) {
-//                return console.log(err);
-//           } else {
+//   connectionDb.query(`SELECT * FROM azienda WHERE id_azienda = '${id}' `, (err, res) => {
+//     if (err) {
+//       return console.log(err);
+//     } else {
 
-//                 console.log(res)
+//       console.log(res)
 
-//             }
-//         })});
+//     }
+//   })
+// });
+
+router.get(`/azienda/:id`, (req, res) => {
+  var id = req.params.id;
+  connectionDb.query(
+    `SELECT * FROM azienda WHERE id_azienda = '${id}' `,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+  
+      } else {
+        console.log(result);
+        res.send(result)
+      }
+    }
+  );
+});
+
 
 module.exports = router;
